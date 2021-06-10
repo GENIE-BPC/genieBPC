12a9330bc42559a6062cb6ce7889bb9d276b13d6
Lavery <laveryj@mskcc.org>
2021-06-10 17:08:57 -0400

    try to document datafile for synapse_tables outside of creating file

---
+++ R/synapse_tables.R
@@ -0,0 +1,14 @@
+#' Synapse table IDs
+#'
+#' A dataset containing the Synapse table IDs for each clinical dataset in GENIE BPC.
+#'
+#' @format A lookup table for Synapse clinical data table IDs:
+#' \describe{
+#'   \item{cohort}{GENIE BPC Project Cohort}
+#'   \item{df}{Clinical dataset}
+#'   \item{version}{Release version}
+#'   \item{synapse_id}{Synapse table ID for each dataset}
+#'   ...
+#' }
+#' @source \url{https://www.synapse.org/#!Synapse:syn21226493/wiki/599164}
+"synapse_tables"
