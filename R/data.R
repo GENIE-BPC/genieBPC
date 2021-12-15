daec49d39bc8f7a2434081dd143dd9fe32a2c506
Brown <BrownS7@mskcc.org>
2021-12-15 13:09:45 -0500

    add documentation for genie_panels

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Genomic Panels Included in GENIE BPC Data
+#'
+#' A dataset containing the name, assay identifier, and number of genes
+#' in each next-generation sequencing targeted panel included in GENIE BPC.
+#'
+#' @format A data frame with 12 rows and 3 variables:
+#' \describe{
+#'   \item{Sequence.Assay.ID}{Next-generation sequencing targeted panel assay identifier}
+#'   \item{Panel}{Panel name}
+#'   \item{Genes}{Number of genes included}
+#'   ...
+#' }
+"genie_panels"
