0fd8efa66dbbfe399e89e91af25ce39e30fbda4e
slb2240 <samanthalbrown8@gmail.com>
2022-02-04 10:13:26 -0500

    Document exported data objects in data.R

---
+++ R/data.R
@@ -0,0 +1,64 @@
+#' List of Drug Regimen Names by Cohort
+#'
+#' A dataset containing the cancer-directed drug names and their synonyms.
+#'
+#' @format A table for cancer-directed drug names associated with each
+#' cancer cohort:
+#' \describe{
+#'   \item{cohort}{GENIE BPC Project cancer. Must be one of "NSCLC"
+#'   (non-small cell lung cancer) or "CRC" (colorectal cancer).
+#'   Future cohorts will include "BrCa" (breast cancer), "PANC"
+#'   (pancreatic cancer), "Prostate" (prostate cancer).}
+#'   \item{drug_name}{Name of generic/ingredient cancer-directed drug}
+#'   \item{drug_name_full}{Name of generic/ingredient cancer-directed drug
+#'   with associated synonyms in parentheses}
+#'   ...
+#' }
+"drug_regimen_list"
+
+
+#' Genomic Panels Included in GENIE BPC Data
+#'
+#' A dataset containing the name, assay identifier, and number of genes
+#' in each next-generation sequencing targeted panel included in GENIE BPC.
+#'
+#' @format A data frame with 12 rows and 3 variables:
+#' \describe{
+#'   \item{Sequence.Assay.ID}{Next-generation
+#'   sequencing targeted panel assay identifier}
+#'   \item{Panel}{Panel name}
+#'   \item{Genes}{Number of genes included}
+#'   ...
+#' }
+"genie_panels"
+
+
+#' List of Drug Regimen Abbreviations
+#'
+#' A dataset containing the cancer-directed drug regimens and their common abbreviations
+#'
+#' @format A table for cancer-directed drug regimens and
+#' their common abbreviations
+#' \describe{
+#'   \item{regimen_drugs}{List of all drugs in the regimen}
+#'   \item{abbreviation}{Common name of drug regimen, e.g. FOLFOX}
+#'   ...
+#' }
+"regimen_abbreviations"
+
+
+#' Synapse table IDs
+#'
+#' A dataset containing the Synapse table IDs for
+#' each clinical dataset in GENIE BPC.
+#'
+#' @format A lookup table for Synapse clinical data table IDs:
+#' \describe{
+#'   \item{cohort}{GENIE BPC Project Cohort}
+#'   \item{df}{Clinical dataset}
+#'   \item{version}{Release version}
+#'   \item{synapse_id}{Synapse table ID for each dataset}
+#'   ...
+#' }
+#' @source \url{https://www.synapse.org/#!Synapse:syn21226493/wiki/599164}
+"synapse_tables"
