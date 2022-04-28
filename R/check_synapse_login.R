b03963f4c2517c22a32e5a7663d0e4b479cfa337
laveryj <laveryj@mskcc.org>
2022-04-28 11:26:28 -0400

    attempt #3 at getting runiverse checks to run
    * removed skip_on_ci() since runiverse was still trying to run checks
    * simplified check_synapse_login() to return TRUE/FALSE, doesn't involve testthat::skip() anymore
    * wrote the skips as three separate skips within each test. one is to check for synapser package, second is to check for synapse login, third is to check for appropriate access to data

--- R/check_synapse_login.R
+++ R/check_synapse_login.R
@@ -10,54 +10,22 @@
 check_synapse_login <- function() {
   if ("synapser" %in% rownames(utils::installed.packages()) == FALSE) {
     t1 <- FALSE
-    return(TRUE)
+    # return(TRUE)
     stop("Please install the package synapser from http://ran.synapse.org")
   } else {
     t1 <- TRUE
   }
-  t2 <- tryCatch(
+  t2 <- try(
     synapser::synLogin(),
-    error =
-      function(e) {
-        cli::cli_alert_warning(
-          paste(
-            "There was an error accessing Synapse.",
-            "See error message below."
-          )
-        )
-        paste("You are not logged into your synapse account",
-              "Please set credentials by using 'synapser::synLogin()'",
-              "To store login credentials in your operating system, call:",
-              "`synLogin(email, password, rememberMe = TRUE)`",
-              "Note: Upon calling `rememberMe = TRUE`, the user can call:",
-              "`synLogin()` in future uses without specifying credentials.",
-              sep = "\n"
-        ) %>%
-          stop(call. = FALSE)
-      }
-  )
-  t3 <- tryCatch(
-    synapser::synGet("syn26948075"),
-    error =
-      function(e) {
-        cli::cli_alert_warning(
-          paste(
-            "There was an error pulling data from Synapse.",
-            "See error message below."
-          )
-        )
-        paste("You are not able to access the data.",
-              "Please make sure you have permission to access",
-              "the GENIE BPC data on Synapse.",
-              sep = "\n"
-        ) %>%
-          stop(call. = FALSE)
-      }
-  )
+   silent = TRUE)
 
-  if (t1 == FALSE || inherits(t2, "try-error") || inherits(t3, "try-error")) {
-    testthat::skip("You do not have synapser installed or do not have access to the data.")
-  } else {
+  t3 <- try(
+    synapser::synGet("syn26948075"), silent = TRUE)
+
+  return( if(t1 == FALSE || inherits(t2,"try-error")||inherits(t3,"try-error")) {
+    FALSE
+  }else{
     TRUE
   }
+  )
 }
--- tests/testthat/test-synapse_version.R
+++ tests/testthat/test-synapse_version.R
@@ -1,5 +1,10 @@
 test_that("Testing synapse version", {
-  skip_on_ci()
+  # exit if user doesn't have synapser, a log in, or access to data.
+  skip_if_not_installed("synapser", minimum_version = NULL)
+  skip_if(inherits(try(synapser::synLogin(), silent = TRUE), "try-error"),
+          "Not logged into Synapse")
+  skip_if(inherits(try(synapser::synGet("syn26948075"), silent = TRUE), "try-error"),
+          "Not able to access the data")
 
   expect_equal(class(synapse_version(FALSE)),
                c("grouped_df", "tbl_df", "tbl", "data.frame"))
