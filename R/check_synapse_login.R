fe9f5116047149d648775976807b967628f240e3
laveryj <laveryj@mskcc.org>
2022-05-05 11:46:37 -0400

    Update check_synapse_login.R

--- R/check_synapse_login.R
+++ R/check_synapse_login.R
@@ -12,9 +12,6 @@ check_synapse_login <- function() {
   if ("synapser" %in% rownames(utils::installed.packages()) == FALSE) {
     t1 <- FALSE
 
-    # changed from stop() to print()
-    # can't be a stop bc then RUniverse returns an error with GH Actions
-    print("Please install the package synapser from http://ran.synapse.org")
   } else { # if synapser is installed, try to login and access the data
     t1 <- TRUE
 
