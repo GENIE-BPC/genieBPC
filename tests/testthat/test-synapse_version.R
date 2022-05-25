ce88d4e71f4b72e460fc4b763e450d37215d845a
laveryj <laveryj@mskcc.org>
2022-05-25 11:34:51 -0400

    Update test-synapse_version.R

--- tests/testthat/test-synapse_version.R
+++ tests/testthat/test-synapse_version.R
@@ -1,10 +1,6 @@
 test_that("Testing synapse version", {
   # exit if user doesn't have synapser, a log in, or access to data.
-  skip_if_not_installed("synapser", minimum_version = NULL)
-  skip_if(inherits(try(synapser::synLogin(), silent = TRUE), "try-error"),
-          "Not logged into Synapse")
-  skip_if(inherits(try(synapser::synGet("syn26948075"), silent = TRUE), "try-error"),
-          "Not able to access the data")
+  testthat::skip_if_not(.check_synapse_login())
 
   expect_equal(class(synapse_version(FALSE)),
                c("grouped_df", "tbl_df", "tbl", "data.frame"))
