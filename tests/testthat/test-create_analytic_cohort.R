e4e987d1cd1d4f55487f51469eff1f314ee4a9ff
michaelcurry1123 <30420015+michaelcurry1123@users.noreply.github.com>
2022-07-22 12:56:28 -0400

    removed words global env

--- tests/testthat/test-create_analytic_cohort.R
+++ tests/testthat/test-create_analytic_cohort.R
@@ -53,7 +53,7 @@ test_that("multiple cohorts- argument check", {
 })
 
 # pull data for each cohort
-# return to global environment to avoid having to re-run pull_data_synapse for
+# return to avoid having to re-run pull_data_synapse for
 # each test
 testthat::expect_true(length(if (.is_connected_to_genie()) {
   nsclc_data <- pull_data_synapse("NSCLC",
