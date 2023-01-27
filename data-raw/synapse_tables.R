0521c6344ac02a00966c0655dfff02e858fde700
laveryj <laveryj@mskcc.org>
2023-01-27 14:58:00 -0500

    update synapse_tables
    Update the order that the tables are read in so that pt_char, ca_dx, etc. are always first regardless of the order the synapse tables are listed in the excel file

--- data-raw/synapse_tables.R
+++ data-raw/synapse_tables.R
@@ -1,7 +1,7 @@
 synapse_tables <- readxl::read_excel("data-raw/synapse_tables.xlsx") %>%
   # TEMPORARY FIX TO KEEP FUNCTIONS WORKING,
   # WILL REMOVE
-  mutate(dataset = case_when(
+  mutate(dataset = factor(case_when(
     dataset == "patient_level_dataset" ~ "pt_char",
     dataset == "cancer_level_dataset_index" ~ "ca_dx_index",
     dataset == "cancer_level_dataset_non_index" ~ "ca_dx_non_index",
@@ -15,7 +15,12 @@ synapse_tables <- readxl::read_excel("data-raw/synapse_tables.xlsx") %>%
     dataset == "data_cna" ~ "cna",
     dataset == "data_fusions" ~ "fusions",
     dataset == "data_mutations_extended" ~ "mutations_extended"
-  )) %>%
+  ),
+  levels = c("pt_char", "ca_dx_index", "ca_dx_non_index",
+             "ca_drugs", "prissmm_imaging", "prissmm_pathology",
+             "ca_radtx", "prissmm_md", "tumor_marker",
+             "cpt", "fusions", "mutations_extended", "cna"))) %>%
+  arrange(release_date, cohort, version, dataset) %>%
   rename(df = dataset)
 
 usethis::use_data(synapse_tables, internal = FALSE, overwrite = TRUE)
