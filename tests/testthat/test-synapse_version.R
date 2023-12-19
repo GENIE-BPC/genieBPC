cd25d24f6828a4c70307dda2d867973a7edbdf13
Jessica Lavery <46568753+jalavery@users.noreply.github.com>
2023-12-19 16:41:58 -0500

    Bladder consortium release / add cohort parameter to `synapse_version()` (#126)
    * add bladder data release, add cohort argument to synapse_version function
    
    * doc updates
    
    * Update test-pull_data_synapse.R
    
    add number of rows and columns for bladder 1.2-consortium
    
    * Add tests for synapse_version
    
    * Update test-synapse_version.R
    
    * Update NEWS.md
    
    * Update synapse_version.Rd
    
    * Update DESCRIPTION
    
    * Revert "Update DESCRIPTION"
    
    This reverts commit 12647ad7c55e5b42db6f00b6beaa1e2a1ed66225.
    
    * Commit to rerun github actions
    
    ---------
    
    Co-authored-by: Brown <samanthalbrown8@gmail.com>
    Co-authored-by: Brown <BrownS7@mskcc.org>

--- tests/testthat/test-synapse_version.R
+++ tests/testthat/test-synapse_version.R
@@ -3,20 +3,49 @@ test_that("Testing synapse version", {
   testthat::skip_if_not(.is_connected_to_genie())
 
   expect_equal(
-    class(synapse_version(FALSE)),
+    class(synapse_version(most_recent = FALSE)),
     c("tbl_df", "tbl", "data.frame")
   )
 
   expect_equal(
-    class(synapse_version(TRUE)),
+    class(synapse_version(most_recent = TRUE)),
     c("tbl_df", "tbl", "data.frame")
   )
 
-  expect_equal(nrow(synapse_version(TRUE)) < nrow(synapse_version(FALSE)),
-               TRUE)
+  expect_equal(
+    nrow(synapse_version(most_recent = TRUE)) <
+      nrow(synapse_version(most_recent = FALSE)),
+    TRUE
+  )
 })
 
 test_that("Test most_recent argument", {
   expect_error(synapse_version(most_recent = "ABC"))
   expect_error(synapse_version(most_recent = 123))
+})
+
+test_that("Test cohort argument", {
+  # if no cohort specified, all cohorts returned
+  expect_equal(nrow(synapse_version()),
+               nrow(synapse_tables %>% distinct(cohort, version)))
+
+  # if cohort is specified, only that cohort is returned
+  # run for each cohort
+  imap(
+    synapse_tables %>% split(.$cohort),
+    ~ expect_equal(
+      nrow(synapse_version(cohort = .y)),
+      nrow(synapse_tables %>%
+             distinct(cohort, version) %>%
+             filter(cohort == .y))
+    )
+  )
+
+  # if multiple cohorts are specified, each cohort is returned
+  expect_equal(
+    nrow(synapse_version(cohort = c("NSCLC", "CRC"))),
+    nrow(synapse_tables %>%
+           distinct(cohort, version) %>%
+           filter(cohort %in% c("NSCLC", "CRC")))
+  )
 })
