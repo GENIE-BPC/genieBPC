c22dfc2709810c4e3fa437a13d25beec60968b3a
jalavery <laveryj@mskcc.org>
2024-07-03 14:24:34 -0400

    update tests to conditionally run

--- tests/testthat/test-set_synapse_credentials.R
+++ tests/testthat/test-set_synapse_credentials.R
@@ -3,30 +3,40 @@ test_that("Invalid function parameter", {
 })
 
 test_that("Nothing supplied, see if PAT used over user/pass", {
+  testthat::skip_if_not(.is_connected_to_genie(pat = Sys.getenv("SYNAPSE_PAT")))
+
   expect_message(
     set_synapse_credentials(), "You are now connected to 'Synapse' with your Personal*"
   )
 })
 
 test_that("set pat = NULL but PAT exists in environment", {
+  testthat::skip_if_not(.is_connected_to_genie(pat = Sys.getenv("SYNAPSE_PAT")))
+
   expect_message(
     set_synapse_credentials(pat = NULL), "You are now connected to 'Synapse' with your Personal*"
   )
 })
 
 test_that("set pat = NULL but PAT exists in environment", {
+  testthat::skip_if_not(.is_connected_to_genie(pat = Sys.getenv("SYNAPSE_PAT")))
+
   expect_error(
     set_synapse_credentials(pat = ""), "*"
   )
 })
 
 test_that("set pat = NULL but PAT exists in environment", {
+  testthat::skip_if_not(.is_connected_to_genie(pat = Sys.getenv("SYNAPSE_PAT")))
+
   expect_error(
     set_synapse_credentials(pat = " "), "*"
   )
 })
 
 test_that("explicitly call user/pass but set wrong and PAT is in environ", {
+  testthat::skip_if_not(.is_connected_to_genie(pat = Sys.getenv("SYNAPSE_PAT")))
+
   expect_error(
     set_synapse_credentials(username = "kitty", password = "puppy"),
     "There was an error authenticating your username (kitty)*"
