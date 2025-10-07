688cf521f44763b032fe073ce4d4f32333143491
jalavery <laveryj@mskcc.org>
2025-10-07 16:50:34 -0400

    update to use csv file instead of tribble for data_releases_expected_size

--- data-raw/regimen_abbreviations.R
+++ data-raw/regimen_abbreviations.R
@@ -1,3 +1,3 @@
-regimen_abbreviations <- read.csv("C:/Users/laveryj/Desktop/genieBPC/data-raw/regimen_abbreviations.csv")
+regimen_abbreviations <- read.csv(here::here("data-raw/regimen_abbreviations.csv"))
 
 usethis::use_data(regimen_abbreviations, overwrite = TRUE)
